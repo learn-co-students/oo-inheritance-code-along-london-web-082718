54c6079035dc54fb137f6d8085471c5966c657cb
braj011 <bhagyaraj01@gmail.com>
2018-09-05 12:43:13 +0100

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,20 @@
 require_relative "./vehicle.rb"
+
+# Notice that we are requiring lib/vehicle.rb. That is because our Car class will need
+# access to the Vehicle class and will therefore need access to the file that
+# contains that class.
+
+
+# Car is the sub-class
+
+class Car < Vehicle
+
+
+  # Looks like the test
+  # is expecting the #go method on an individual car to return the phrase:
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
